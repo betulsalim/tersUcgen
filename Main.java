0e1003fa35570bc9717d8d889bb1558fec5f57df
betulsalim <112887429+betulsalim@users.noreply.github.com>
2022-10-31 13:21:04 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        int i,sayi,j,k,l;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Basamak sayısını giriniz: ");
+        sayi = input.nextInt();
+
+        for(i=sayi;i>=1;i--){
+            for(j=sayi-i;j>=1;j--){
+                System.out.print(" ");
+            }
+            for(k=(2*i-1);k>=1;k--){
+                System.out.print("*");
+        }
+            System.out.println(" ");
+
+        }
+
+
+    }
+}
